06fc2c55dafea9f752a52675527a6d1895bf70eb
Jin Lee <jinslee@gmail.com>
2026-03-28 09:32:50 -0700

    Update Locus to v0.2.0

--- Casks/locus.rb
+++ Casks/locus.rb
@@ -1,6 +1,6 @@
 cask "locus" do
-  version "0.1.6"
-  sha256 "0f3ad4c1ae4c331a83207007ce77211e9ffe0467b941affc469516935150518a"
+  version "0.2.0"
+  sha256 "dcf25e7ff3bc6d1043b5bb1ad376848c8dcdb5d550db8fd2d39922f4c46dade6"
 
   url "https://github.com/entigrate/locus/releases/download/v#{version}/Locus-#{version}.dmg"
   name "Locus"
